5ba2608c21c8b52f70464ca7c96ca99f24eeea29
mio <yama_moto0525@keio.jp>
2025-07-28 19:03:41 +0900

    setup ogp

--- setting.ts
+++ setting.ts
@@ -1,2 +1,2 @@
-// ex) export const TARGET_URL = "https://xxxx.github.io/tech-blog.github.io/";
+// ex) export const TARGET_URL = "https://mio12-12.github.io/tech-blog.github.io/";
 export const TARGET_URL = "https://";
